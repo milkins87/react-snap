7025ed75b0b9a8d30baa624a09cf242ddea95e21
milenavojinovic <milena@yamo.ch>
2020-05-12 16:11:20 +0200

    set timeout to 0 in puppeteer_utils

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -250,7 +250,11 @@ const crawl = async opt => {
         await page.setUserAgent(options.userAgent);
         const tracker = createTracker(page);
         try {
-          await page.goto(pageUrl, { waitUntil: "networkidle0" });
+          await page.goto(pageUrl, {
+            networkIdleTimeout: 5000,
+            waitUntil: 'networkidle',
+            timeout: 0
+          });
         } catch (e) {
           e.message = augmentTimeoutError(e.message, tracker);
           throw e;
