2adb235df4a1269c7b5c2a3149c8b9687d1ea6ef
milenavojinovic <milena@yamo.ch>
2020-05-19 11:59:32 +0200

    set timeout to 0

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -250,7 +250,7 @@ const crawl = async opt => {
         await page.setUserAgent(options.userAgent);
         const tracker = createTracker(page);
         try {
-          await page.goto(pageUrl, { waitUntil: "networkidle2" });
+          await page.goto(pageUrl, { waitUntil: "networkidle2", timeout: 0 });
         } catch (e) {
           e.message = augmentTimeoutError(e.message, tracker);
           throw e;
